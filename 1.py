4c5e97dfcb9e2221799c23b0e8c96dbecdbc100b
shreyaudupa <shreyaudupa26@gmail.com>
2017-01-13 12:05:01 +0530

    done

--- 1.py
+++ 1.py
@@ -2,10 +2,16 @@ def prime_num(*list1):
 	print list1
 	return list1
 
-n=input("enter the range")
-
-for i in range(n):
-  if i%2==0:
-     print "its prime"
-     list.append(i)
-prime_num(list)
+no=input("enter the range")
+list2= []
+for num in range(no):
+  if num>1:
+   
+    for i in range(2,num):
+	#print num
+        if num%i==0:
+         break
+    else:
+        list2.append(num)
+	#print num
+prime_num(list2)
