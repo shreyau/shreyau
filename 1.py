ee4940a01a8b145ee2ece0b39f6a1a1270adf721
shreyaudupa <shreyaudupa26@gmail.com>
2017-01-13 11:32:32 +0530

    done

--- 1.py
+++ 1.py
@@ -1,1 +1,11 @@
-print "hello"
+def prime_num(*list1):
+	print list1
+	return list1
+
+n=input("enter the range")
+
+for i in range(n):
+  if i%2==0:
+     print "its prime"
+     list.append(i)
+prime_num(list)
